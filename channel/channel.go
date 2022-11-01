ca7195c5b67c34af96544dfc26899321c4630776
luozhen2020 <rosechild2010@sina.com>
2022-11-01 23:15:28 +0800

    1

--- channel/channel.go
+++ channel/channel.go
@@ -24,21 +24,27 @@ func chanDemo() {
 
 func createWorker(id int) chan<- int {
 	c := make(chan int)
-	go func() {
-		for {
-			fmt.Printf("Worker %d received %c\n", id, <-c)
-		}
-	}()
+	go worker(id, c)
 
 	return c
+}
+
+func worker(id int, c chan int) {
+	for {
+		fmt.Printf("Worker %d received %c\n", id, <-c)
+	}
 }
 
 func bufferedChannel() {
 	c := make(chan int, 3)
 
-	c <- 1
-	c <- 2
-	c <- 3
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+
+	time.Sleep(time.Millisecond)
 }
 
 func main() {
