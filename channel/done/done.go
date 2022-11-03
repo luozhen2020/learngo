36bb2acb999ae77c95cab0ab5f3d74352542e520
luozhen2020 <rosechild2010@sina.com>
2022-11-03 22:44:19 +0800

    1

---
+++ channel/done/done.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+func chanDemo() {
+	var workers [10]worker
+	for i := 0; i < 10; i++ {
+		workers[i] = createWorker(i)
+	}
+
+	for i := 0; i < 10; i++ {
+		workers[i].in <- 'a' + i
+		<-workers[i].done
+	}
+
+	for i := 0; i < 10; i++ {
+		workers[i].in <- 'A' + i
+		<-workers[i].done
+	}
+
+	/*time.Sleep(time.Millisecond)*/
+}
+
+func createWorker(id int) worker {
+	w := worker{
+		in:   make(chan int),
+		done: make(chan bool),
+	}
+	go doWork(id, w)
+
+	return w
+}
+
+type worker struct {
+	in   chan int
+	done chan bool
+}
+
+func doWork(id int, w worker) {
+	for n := range w.in {
+		/*n, ok := <-c
+		if !ok {
+			break
+		}*/
+		fmt.Printf("Worker %d received %c\n", id, n)
+		w.done <- true
+	}
+}
+
+func main() {
+	chanDemo()
+}
