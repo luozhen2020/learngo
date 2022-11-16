7ca99108be5ab506d32ae8cc36f9a3ac1ec6bf16
luozhen2020 <rosechild2010@sina.com>
2022-11-16 20:14:07 +0800

    1

--- channel/done/done.go
+++ channel/done/done.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func chanDemo() {
+/*func chanDemo() {
 	var workers [10]worker
 	for i := 0; i < 10; i++ {
 		workers[i] = createWorker(i)
@@ -12,24 +13,47 @@ func chanDemo() {
 
 	for i := 0; i < 10; i++ {
 		workers[i].in <- 'a' + i
-		/*<-workers[i].done*/
+		//<-workers[i].done
+	}
+
+	for _, worker := range workers {
+		<-worker.done
 	}
 
 	for i := 0; i < 10; i++ {
 		workers[i].in <- 'A' + i
-		/*<-workers[i].done*/
+		//<-workers[i].done
 	}
 
-	/*time.Sleep(time.Millisecond)*/
+	//time.Sleep(time.Millisecond)
 
 	// wait for all of them
 	for _, worker := range workers {
 		<-worker.done
-		<-worker.done
 	}
+}*/
+
+func chanDemo() {
+	var workers [10]worker
+	group := sync.WaitGroup{}
+
+	for i := 0; i < 10; i++ {
+		workers[i] = createWorker(i, &group)
+	}
+	group.Add(20)
+
+	for i := 0; i < 10; i++ {
+		workers[i].in <- 'a' + i
+	}
+
+	for i := 0; i < 10; i++ {
+		workers[i].in <- 'A' + i
+	}
+
+	group.Wait()
 }
 
-func createWorker(id int) worker {
+/*func createWorker(id int) worker {
 	w := worker{
 		in:   make(chan int),
 		done: make(chan bool),
@@ -37,24 +61,42 @@ func createWorker(id int) worker {
 	go doWork(id, w)
 
 	return w
+}*/
+
+func createWorker(id int, wg *sync.WaitGroup) worker {
+	w := worker{
+		in: make(chan int),
+		done: func() {
+			wg.Done()
+		},
+	}
+	go doWork(id, w)
+
+	return w
 }
+
+/*type worker struct {
+	in   chan int
+	done chan bool
+}*/
 
 type worker struct {
 	in   chan int
-	done chan bool
+	done func()
 }
+
+/*func doWork(id int, w worker) {
+	for n := range w.in {
+		fmt.Printf("Worker %d received %c\n", id, n)
+		w.done <- true
+
+	}
+}*/
 
 func doWork(id int, w worker) {
 	for n := range w.in {
-		/*n, ok := <-c
-		if !ok {
-			break
-		}*/
 		fmt.Printf("Worker %d received %c\n", id, n)
-		go func() {
-			w.done <- true
-		}()
-
+		w.done()
 	}
 }
 
