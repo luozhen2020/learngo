50e47284a4cb918467baaf7cca475618f9a75777
luozhenGitHub <rosechild2005@sina.com>
2023-02-23 22:49:56 +0800

    xyy

--- crawler/main.go
+++ crawler/main.go
@@ -1,58 +1,13 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/transform"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+	"xyy/learngo/crawler/engine"
+	"xyy/learngo/crawler/zhenai/parser"
 )
 
 func main() {
-	resp, err := http.Get("https://www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error: status code", resp.StatusCode)
-		return
-	}
-	fmt.Println("xyy!")
-	e := determineEncoding(resp.Body)
-
-	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
-	all, err := ioutil.ReadAll(utf8Reader)
-	if err != nil {
-		panic(err)
-	}
-	//fmt.Printf("%s\n", all)
-	printCityList(all)
-}
-
-func determineEncoding(r io.Reader) encoding.Encoding {
-	bytes, err := bufio.NewReader(r).Peek(1024)
-	if err != nil {
-		panic(err)
-	}
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
-}
-
-func printCityList(contents []byte) {
-	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
-	matches := re.FindAllSubmatch(contents, -1) // returns [][][]byte
-	for _, m := range matches {
-		/*for _, submatch := range m {
-			fmt.Printf("%s ", submatch)
-		}
-		fmt.Println()*/
-		fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
-	}
-	fmt.Printf("Matches found: %d\n", len(matches))
+	engine.Run(engine.Request{
+		Url:        `https://www.zhenai.com/zhenghun`,
+		ParserFunc: parser.ParseCityList,
+	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module xyy/learngo
 go 1.16
 
 require (
-	golang.org/x/net v0.5.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/net v0.6.0
+	golang.org/x/text v0.7.0
 )
